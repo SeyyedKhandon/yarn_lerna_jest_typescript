aed21ccc8880d3bddf4530defda72e064a897b63
Seyyed Mehdi Hassanpour <seyyedkhandon@gmail.com>
2020-07-04 11:57:15 +0430

    feat: add prerelease command to yarn release, and update common

--- packages/common/lib/index.js
+++ packages/common/lib/index.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.addBy20 = exports.addBy10 = exports.add = void 0;
+exports.addBy40 = exports.addBy30 = exports.addBy20 = exports.addBy10 = exports.add = void 0;
 exports.add = (a, b) => a + b;
 exports.addBy10 = (a) => a + 10;
 exports.addBy20 = (a) => a + 20;
+exports.addBy30 = (a) => a + 30;
+exports.addBy40 = (a) => a + 40;
 //# sourceMappingURL=index.js.map
